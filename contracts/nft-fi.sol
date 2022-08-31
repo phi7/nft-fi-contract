aa295edce3fb683b98668116bba204337610ea5c
intern2022-020 <intern2022-020@YINN1333.GMO.LOCAL>
2022-08-31 18:21:21 +0900

    データ構造変更

--- contracts/nft-fi.sol
+++ contracts/nft-fi.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 import "./nft_mint.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 interface INftMint {
     function transferFrom(
@@ -13,13 +14,31 @@ interface INftMint {
         uint256 tokenId
     ) external;
 
-    function safetransferFrom(
+    // function approve(address to, uint256 tokenId) external;
+
+    // function safetransferFrom(
+    //     address _from,
+    //     address _to,
+    //     uint256 tokenId
+    // ) external;
+
+
+}
+
+interface IGyen {
+    function transferFrom(
         address _from,
         address _to,
-        uint256 tokenId
+        uint256 _value
     ) external;
 
+    // function approve(address spender, uint256 amount) external;
 
+    // function safetransferFrom(
+    //     address _from,
+    //     address _to,
+    //     uint256 tokenId
+    // ) external;
 }
 
 contract Nftfi {
@@ -28,11 +47,20 @@ contract Nftfi {
     //     console.log("This is my NFT contract.");
     // }
     INftMint nftMint;
+    IGyen gyen;
+
+    using SafeMath for uint256;
 
     constructor(){
         // address nftMintAdrr = _nftMintAdrr;
         nftMint = INftMint(0x842BDfd7da2d603b176f0E41B58a6f2D785aFBcA);
+        gyen = IGyen(0x2d74278AF15427f2b8216Ad29C947e44B86D3934);
     }
+
+    // 融資金額の５％は投資者へ返還
+    uint256 profitToInvester = 5;
+    // 融資金額の５％はプラットフォームへ返還
+    uint256 profitToPlatform = 5;
 
     //担保に出されたNFTの情報
     struct CollateralizedNFTInfo {
@@ -42,8 +70,12 @@ contract Nftfi {
         address nftContractAddress;
         uint256 tokenId;
         uint256 timestamp;
+        uint256 auctionDuration;
+        uint256 loanStartTime;
+        uint256 loanDuration;
         address invester;
-        uint256 price;
+        bool isBidding;
+        uint256 biggestBidPrice;
         bool isCollateralizing;
     }
 
@@ -62,7 +94,7 @@ contract Nftfi {
         payable
     {
         uint256 timestamp = block.timestamp;
-        uint256 initialPrice = 100;
+        // uint256 initialPrice = 100;
         uint256 collateralizerNFTListLength = collateralizedNFTs.length;
         CollateralizedNFTInfo
             memory newCollateralizedNFT = CollateralizedNFTInfo(
@@ -71,8 +103,17 @@ contract Nftfi {
                 _nftContractAddress,
                 _tokenId,
                 timestamp,
+                //オークションの期間は１週間
+                1 weeks,
+                //運用開始時間は最初は０
+                0,
+                //ローンの期間は１週間
+                1 weeks,
                 address(0),
-                initialPrice,
+                //isBidding
+                false,
+                //最初の最大の価格はinitialPriceと一致
+                100,
                 true
             );
         // collateralizerNFTList[msg.sender].push(newCollateralizedNFT);
@@ -83,7 +124,8 @@ contract Nftfi {
         // return collateralizedNFTs;
         // transferFrom();
         console.log("caller %s",msg.sender);
-        // nftMint.transferFrom(msg.sender, address(this), _tokenId);
+        // nftMint.approve(address(this), _tokenId);
+        nftMint.transferFrom(msg.sender, address(this), _tokenId);
         // nftMint.safetransferFrom(msg.sender, address(this), _tokenId);
 
         // CollateralizeInfo[counter] = CollateralizedNFTInfo(
@@ -104,9 +146,53 @@ contract Nftfi {
     }
 
     //投資家がNFTを入札する関数
-    function makeBid(uint256 countIndex, uint256 price) external {
-        require(collateralizedNFTs[countIndex].price < price);
+    function makeBid(uint256 countIndex, uint256 price) external payable {
+        require(collateralizedNFTs[countIndex].biggestBidPrice < price);
         collateralizedNFTs[countIndex].invester = msg.sender;
-        collateralizedNFTs[countIndex].price = price;
+        collateralizedNFTs[countIndex].biggestBidPrice = price;
+        collateralizedNFTs[countIndex].isBidding = true;
+        // gyen.approve(address(this), price);
+        gyen.transferFrom(msg.sender, address(this), price);
+    }
+
+    //NFTホルダーが落札を決定する関数
+    function accept(uint256 countIndex) external payable {
+        require(collateralizedNFTs[countIndex].collateralizer == msg.sender, "not owner of the token");
+        //最低一人は入札しているかどうか
+        require(collateralizedNFTs[countIndex].isBidding,"not yet bidding");
+        //ローンの開始時間を記録
+        collateralizedNFTs[countIndex].loanStartTime = block.timestamp;
+        gyen.transferFrom(address(this),collateralizedNFTs[countIndex].collateralizer,collateralizedNFTs[countIndex].biggestBidPrice);
+    }
+
+    //返済するための関数
+    //ローン期間が過ぎてなければNFTを戻す
+    function payback(uint256 countIndex) external payable{
+        //呼び出した人がNFTを担保に出した人か確認
+        require(collateralizedNFTs[countIndex].collateralizer == msg.sender, "not owner of the token");
+        //ローンの期間がすぎていないか確認
+        require(block.timestamp <= collateralizedNFTs[countIndex].loanStartTime.add(collateralizedNFTs[countIndex].loanDuration) );
+        nftMint.transferFrom(address(this), msg.sender, collateralizedNFTs[countIndex].tokenId);
+        //ここでaproveしてから利息付きのGyenをNFTホルダーから投資家にtransferfromしてもよい
+    }
+
+    //ローン期間がすぎたけど返済されなかったので清算する関数
+    function liquidate(uint256 countIndex) external payable{
+        //ローン期間がすぎたかをチェック
+        require(block.timestamp < collateralizedNFTs[countIndex].loanStartTime.add(collateralizedNFTs[countIndex].loanDuration),"yet loan continues..." );
+        nftMint.transferFrom(address(this), collateralizedNFTs[countIndex].invester, collateralizedNFTs[countIndex].tokenId);
+    }
+
+    //入札がなく、オークション期間が終わった場合
+    function withdraw(uint256 countIndex) external payable{
+        //呼び出した人がNFTを担保に出した人か確認
+        require(collateralizedNFTs[countIndex].collateralizer == msg.sender, "not owner of the token");
+        //入札が入っていないか
+        require(!collateralizedNFTs[countIndex].isBidding,"Already Bidded...");
+        //オークション期間が過ぎたか
+        require(collateralizedNFTs[countIndex].timestamp.add(collateralizedNFTs[countIndex].auctionDuration) < block.timestamp, "Still auction time");
+        nftMint.transferFrom(address(this), msg.sender, collateralizedNFTs[countIndex].tokenId);
+
+
     }
 }
